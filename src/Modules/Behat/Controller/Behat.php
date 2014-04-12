0bb199fe9aa600741126a69eca00b9646e310e0c
Golden Dave <dave@theboss.com>
2014-04-12 23:08:58 +0100

    updated controller for init and exec

--- src/Modules/Behat/Controller/Behat.php
+++ src/Modules/Behat/Controller/Behat.php
@@ -7,19 +7,14 @@ class Behat extends Base {
     protected function defaultExecution($pageVars) {
 
         if ($pageVars["route"]["action"] == "init" || $pageVars["route"]["action"] == "initialize") {
-            echo "dickface" ;
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Initializer") ;
-            // if we don't have an object, its an array of errors
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-        }
+            $modGroup = "Initializer" ; }
 
         if ($pageVars["route"]["action"] == "exec" || $pageVars["route"]["action"] == "execute") {
-            echo "shitface" ;
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Executor") ;
-            // if we don't have an object, its an array of errors
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-        }
+            $modGroup = "Executor" ; }
 
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, $modGroup) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
         $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
         if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
 
