62ad56cab3a24ee7805e3ab56f04b87dcf677c34
David Amanshia <phpengine@hotmail.co.uk>
2014-04-12 22:36:17 +0100

    added exec to default actions

--- src/Core/Base/Info/Base.php
+++ src/Core/Base/Info/Base.php
@@ -12,7 +12,7 @@ class Base {
     }
 
     public function routesAvailable() {
-      return array("help", "status", "install", "uninstall", "initialize", "init", "execute");
+      return array("help", "status", "install", "uninstall", "initialize", "init", "execute", "exec");
       // @todo some app settings class should contain this instead
     }
 
