8dd9195354630572ae275fc6ab5745a1bf640be2
David Amanshia <phpengine@hotmail.co.uk>
2014-09-05 12:12:45 +0100

    phpunit exec works correctly now#

--- src/Modules/PHPUnit/Model/PHPUnitExecAllLinux.php
+++ src/Modules/PHPUnit/Model/PHPUnitExecAllLinux.php
@@ -21,71 +21,16 @@ class PHPUnitExecAllLinux extends BaseTestExec {
         $this->autopilotDefiner = "PHPUnitExec";
         $this->installCommands = array(
             "mkdir -p build/tests/phpunit/",
-            "cd build/tests/phpunit/" );
-        $this->uninstallCommands = array(
-            "rm -rf build/tests/phpunit/" );
-        $this->registeredPostInstallFunctions = array(
-            "addTemplatesForFirstFeature",
-            "addTemplatesForFirstFeatureContext" );
+            "cd build/tests/phpunit/",
+            "phpunit .",
+            "cd ".getcwd());
+        $this->uninstallCommands = array( );
+        $this->registeredPostInstallFunctions = array( );
         $this->programNameMachine = "phpunit"; // command and app dir name
         $this->programNameFriendly = " PHPUnit "; // 12 chars
         $this->programNameInstaller = "PHPUnit";
         $this->programExecutorTargetPath = 'phpunit/bin/phpunit';
         $this->initialize();
     }
-
-    protected function addTemplatesForFirstFeature() {
-        $templatorFactory = new \Model\Templating();
-        $templator = $templatorFactory->getModel($this->params);
-        $originalTemplate = dirname(__FILE__)."/../Templates/first-feature.tpl.php" ;
-        $appPath = $this->findPathOfApp() ;
-        $targetLocation = $appPath."build/tests/phpunit/features/first.feature" ;
-        $templator->template(
-            $originalTemplate,
-            array(),
-            $targetLocation );
-    }
-
-    protected function addTemplatesForFirstFeatureContext() {
-        $templatorFactory = new \Model\Templating();
-        $templator = $templatorFactory->getModel($this->params);
-        $originalTemplate = dirname(__FILE__)."/../Templates/FeatureContext.php" ;
-        $appPath = $this->findPathOfApp() ;
-        $targetLocation = $appPath."build/tests/phpunit/features/bootstrap/FeatureContext.php" ;
-        $appUrl = $this->findUrlOfApp() ;
-        $replacements = array( 'site_url' => $appUrl ) ;
-        $templator->template(
-            $originalTemplate,
-            $replacements,
-            $targetLocation );
-    }
-
-    protected function findUrlOfApp() {
-        if (isset($this->params["phpunit-target-url"]) && $this->params["phpunit-target-url"] != "") {
-            $urlOfApp = $this->params["phpunit-target-url"] ; }
-//        else if (isset($this->params["phpunit-environment"]) && $this->params["phpunit-environment"] != "") {
-//            $urlOfApp = $this->getUrlFromPapyrus($this->params["phpunit-environment"]) ; }
-        return $urlOfApp = (isset($urlOfApp)) ? $urlOfApp : null  ;
-    }
-
-    protected function findPathOfApp() {
-        if (isset($this->params["phpunit-target-path"]) && $this->params["phpunit-target-path"] != "") {
-            $pathOfApp = $this->params["phpunit-target-path"] ; }
-//        else if (isset($this->params["phpunit-environment"]) && $this->params["phpunit-environment"] != "") {
-//            $pathOfApp = $this->getPathFromPapyrus($this->params["phpunit-environment"]) ; }
-        if (isset($pathOfApp)) {
-            echo $pathOfApp."\n" ;
-            echo substr($pathOfApp, -1, 1)."\n" ;
-            if (substr($pathOfApp, -1, 1) != '/') { $pathOfApp .= '/' ; } }
-        return (isset($pathOfApp)) ? $pathOfApp : null  ;
-    }
-
-//    protected function getUrlFromPapyrus($environment) {
-//        return "www.papyrus-url.co.uk" ;
-//    }
-//
-//    protected function getPathFromPapyrus($environment) {
-//        return "/var/www/papyrus-output/" ;
-//    }
 
 }
