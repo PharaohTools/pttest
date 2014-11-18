ceb5f9afe6001029345b04b46f6c63747528192b
David Amanshia <phpengine@hotmail.co.uk>
2014-11-18 01:15:51 +0000

    behat extra params

--- src/Modules/Behat/Model/BehatExecutorAllLinux.php
+++ src/Modules/Behat/Model/BehatExecutorAllLinux.php
@@ -19,16 +19,42 @@ class BehatExecutorAllLinux extends BaseTestExec {
         parent::__construct($params);
         $this->paramsForBootstrappingModels = $params ;
         $this->autopilotDefiner = "Behat";
-        $this->installCommands = array(
-            "cd ".getcwd(),
-            "cd build/tests/behat/",
-            "behat" );
+        $this->installCommands = $this->getInstallCommands();
         $this->uninstallCommands = array(  );
         $this->programNameMachine = "behat"; // command and app dir name
-        $this->programNameFriendly = " Behat "; // 12 chars
+        $this->programNameFriendly = "   Behat!   "; // 12 chars
         $this->programNameInstaller = "Behat";
         $this->programExecutorTargetPath = 'behat/bin/behat';
         $this->initialize();
+    }
+
+    private function getInstallCommands() {
+        $featureDir = $this->getFeatureDir() ;
+        $outDir = $this->getOutDir() ;
+        $outType = $this->getOutType() ;
+        $ray = array(
+            "cd ".getcwd(),
+            "cd $featureDir",
+            "behat --format $outType --out $outDir" );
+        return $ray ;
+    }
+
+    private function getFeatureDir() {
+        if (isset($this->params["test-dir"])) { $fDir = $this->params["test-dir"] ; }
+        else { $fDir = "build/tests/behat" ;   }
+        return $fDir ;
+    }
+
+    private function getOutDir() {
+        if (isset($this->params["out-dir"])) { $oDir = $this->params["out-dir"] ; }
+        else { $oDir = "build/reports/behat" ;   }
+        return $oDir ;
+    }
+
+    private function getOutType() {
+        if (isset($this->params["output-type"])) { $oType = $this->params["output-type"] ; }
+        else { $oType = ",html" ;   }
+        return $oType ;
     }
 
 }
